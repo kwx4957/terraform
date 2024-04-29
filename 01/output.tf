2e8bc4628f36be0852eb8232172ac0a790aba63e
kwx4957 <kdy4957@naver.com>
2024-04-29 11:53:58 +0900

    Style : 탭 사이즈 수정
    1. output.tf
    2. compute.tf

--- 01/output.tf
+++ 01/output.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-    value = aws_instance.web_server.public_ip
-    description = "public ip address of nginx web server"
+  value       = aws_instance.web_server.public_ip
+  description = "public ip address of nginx web server"
 }
