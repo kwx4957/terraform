bd49a056df2791bc79b02fe705081dedb4687130
kwx4957 <kdy4957@naver.com>
2024-04-29 11:58:48 +0900

    Feat: 보안 그룹 변수 및 포트 추가
    1. variable.tf
    - https 및 ssh
    2. sg.tf
    - ingress https 및 ssh 추가
    - egress 설정

--- 01/sg.tf
+++ 01/sg.tf
@@ -1,11 +1,36 @@
-resource "aws_security_group" "web_server" { 
-    name = "web-server-asg"
+resource "aws_security_group" "web_server" {
+  name = "web-server-asg"
 
-    ingress {
-        from_port = var.web_server_port
-        to_port = var.web_server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port        = var.web_server_http_port
+    to_port          = var.web_server_http_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+
+  ingress {
+    from_port        = var.ssh_port
+    to_port          = var.ssh_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port   = var.web_server_https_port
+    to_port     = var.web_server_https_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
 }
--- 01/variable.tf
+++ 01/variable.tf
@@ -1,5 +1,17 @@
-variable "web_server_port" {
-    default = "8080"
-    description = "nginx web-server port ingress"
-    type = number
+variable "web_server_http_port" {
+  default     = "80"
+  description = "nginx web-server http port ingress"
+  type        = number
+}
+
+variable "ssh_port" {
+  default     = "22"
+  description = "ssh"
+  type        = number
+}
+
+variable "web_server_https_port" {
+  default     = "443"
+  description = "nginx web-server https port ingress"
+  type        = number
 }
